a7e7fc52ea060bb7846d9c6aa6238b07b1b68a2b
ElDuddz <elduddz@outlook.com>
2019-08-18 00:46:30 +0100

    sparates jobname and spokename

--- Generate-yml.ps1
+++ Generate-yml.ps1
@@ -1,5 +1,5 @@
 $job = @"
-- job: __spokename__
+- job: __jobname__
   steps:
   - task: AzureCLI@1
     inputs:
@@ -30,7 +30,9 @@ foreach($spoke in $spokes) {
   $jobName = $bits[0]
 	$environment = $bits[1]
 	$spokeJob = $job
-	$spokeJob = $spokeJob.replace("__spokename__", $jobName)
+
+	$spokeJob = $spokeJob.replace("__jobname__", $jobName)
+	$spokeJob = $spokeJob.replace("__spokename__", $spoke.name)
 	$spokeJob = $spokeJob.replace("__KVname__", "Enterprise$environment-KV")
 	
 	$yml += $spokeJob
